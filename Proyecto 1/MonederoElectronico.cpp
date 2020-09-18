4f0bac322620a62cb2835f58dce55b793b1b2269
Giancarlo Chaves <giank.ch.m7645@gmail.com>
2020-09-18 09:50:56 -0600

    Desgloce Vuelto
    No es funcional pero revisen por si quieren cambiar algo

--- Proyecto 1/MonederoElectronico.cpp
+++ Proyecto 1/MonederoElectronico.cpp
@@ -9,7 +9,35 @@ MonederoElectronico::MonederoElectronico(float montoInicial){
 }
 
 string MonederoElectronico::desgloceVuelto(string& compra){
-	return string();
+	stringstream r(compra);
+	/*r << "Monto Total: " << totalCompra << endl;
+	r << "Monto Pagado: " << montoPago << endl;
+	r << "---------------Desgloce de Vuelto---------------" << endl;
+	int vuelto = montoPago - totalCompra;
+	int contador = 0;
+	while (vuelto > 0) {
+		if (vuelto % 2000) {
+			for (int i = vuelto; i % 2000; i - 2000)
+				contador += 1;
+			r << contador << "Billete(s) de 2000." << endl;
+		}
+		if (vuelto % 1000) {
+			for (int i = vuelto; i % 1000; i - 1000)
+				contador += 1;
+			r << contador << "Billete(s) de 1000." << endl;
+		}
+		if (vuelto % 500) {
+			for (int i = vuelto; i % 500; i - 500)
+				contador += 1;
+			r << contador << "Moneda(s) de 500." << endl;
+		}
+		if (vuelto % 100) {
+			for (int i = vuelto; i % 100; i - 100)
+				contador += 1;
+			r << contador << "Moneda(s) de 100." << endl;
+		}
+	}*/
+	return r.str();
 }
 
 void MonederoElectronico::setDinero(int dinero){
